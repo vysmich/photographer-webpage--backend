b60bf25aa02e0811c46698a84b88c6e58f4f4c33
Michal Vyskočil <michal.vyskocil@q2.cz>
2022-08-21 21:28:12 +0200

    edit config for graphl

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -26,4 +26,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+    introspection: true,
+    apolloServer: {
+      tracing: false,
+    },
+  },
 });
