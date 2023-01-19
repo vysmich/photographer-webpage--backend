953942b4f6d29a6ec3b816572a65ba186588dc42
Michal Vyskočil <MichalVyskocil@q2cz.local>
2023-01-19 22:39:54 +0100

    back in main

---
+++ src/api/aboutpage/controllers/aboutpage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  aboutpage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::aboutpage.aboutpage');
---
+++ src/api/aboutpage/services/aboutpage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aboutpage service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::aboutpage.aboutpage');
